cfb025a3cce7166739a7ece8c2e11148c67d3ca4
bhaskarkamma5 <bhaskarkamma5@gmail.com>
2019-06-24 20:31:06 +0530

    second commit

--- Project1/src/MyClass.java
+++ Project1/src/MyClass.java
@@ -1,9 +1,15 @@
 
 public class MyClass {
-
+	public int sno;
+	public int getSno() {
+		return sno;
+	}
+	public void setSno(int sno) {
+		this.sno = sno;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		public int sno;
+		
 
 	}
 
